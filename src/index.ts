f3e1cd0c740d9595ae14191e94d7e6fe05291320
SvenFrankson <sven.frankson@gmail.com>
2023-04-22 14:17:01 +0200

    Initial commit.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-export const Greeter = (name: string) => {
-    return "Hello " + name;
+import { MeshBuilder } from '@babylonjs/core';
+import { StandardMaterial } from '@babylonjs/core/Materials/standardMaterial';
+import { Scene } from '@babylonjs/core/scene';
+
+export const MakeBall = (name: string, scene: Scene) => {
+    console.log("It's my ball.");
+    
+    let ball = MeshBuilder.CreateSphere(name, { diameter: 2 }, scene);
+    
+    let mat = new StandardMaterial("material", scene);
+    mat.diffuseColor.copyFromFloats(1, 0, 0);
+    ball.material = mat;
+
+    return ball;
 }
